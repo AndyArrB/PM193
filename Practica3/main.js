522667d35df37361c29eaf1e6756a4881090c4e3
AndyArrB <122043862@upq.edu.mx>
2025-05-22 17:24:41 -0600

    Ejercicio A de la Práctica 3

---
+++ Practica3/main.js
@@ -0,0 +1,5 @@
+import { restar } from "./utils.js";
+
+console.log(restar(8, 3));
+console.log(restar(10, 5));
+console.log(restar(15, 7));
---
+++ Practica3/utils.js
@@ -0,0 +1,8 @@
+//Creo mi función que restara a y b
+var a, b;
+
+function restar(a, b) {
+    return a - b;
+}
+
+export {restar};
