8d153e08439f6f5f4e2dcd966b48e25a6615ab63
AndyArrB <122043862@upq.edu.mx>
2025-05-22 18:11:11 -0600

    Ejercicio A - Práctica 3

--- Practica3/main.js
+++ Practica3/main.js
@@ -1,5 +1,7 @@
+// menciono que aqui usaré el archivo y lo importo
 import { restar } from "./utils.js";
 
+// muestro la ejecución de la función en la consola
 console.log(restar(8, 3));
 console.log(restar(10, 5));
 console.log(restar(15, 7));
--- Practica3/utils.js
+++ Practica3/utils.js
@@ -1,8 +1,11 @@
-//Creo mi función que restara a y b
+//Declaro mis variables
 var a, b;
 
+// creo la función que retorne la resta de dos números
 function restar(a, b) {
     return a - b;
 }
 
+// le indico que exportaré la función
+// y será usada en otro archivo
 export {restar};
