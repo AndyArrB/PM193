899f15d867e9300a3803ba2008698f586bb0e24a
AndyArrB <122043862@upq.edu.mx>
2025-06-12 16:55:54 -0600

    Práctica 5. Finalizada

--- Blank/App.js
+++ Blank/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
+// Texto que cambia de Hola Mundo a Estado Cambiado
 const Texto = () => {
   const [contenido, setContenido] = useState('Hola mundo')
   const actualizaTexto = () => {setContenido('Estado modificado')}
@@ -11,16 +12,27 @@ const Texto = () => {
   )
 }
 
+// Boton con cambio de estado de un Presionar a Presionado
+const PresionarBoton = () => {
+  const [titulo, setTitulo] = useState('Presionar');
+  const actualizarAhora = () => {
+    setTitulo('¡Presionado!');
+  };
+  return (
+      <Button title={titulo} onPress={actualizarAhora} />
+  );
+}
+
 /* ZONA 2: MAIN */
 export default function App() {
   return (
     /* Todos los componentes deben ir dentro de un view */
     <View style={styles.container}>
 
-      <Texto > </Texto>
-      <Texto > </Texto>
-      <Texto > </Texto>
-      <Button title='Presionar'> </Button>
+      <Texto> </Texto>
+      <Texto> </Texto>
+      <Texto> </Texto>
+      <PresionarBoton></PresionarBoton>
       <StatusBar style="auto" />
 
     </View>
