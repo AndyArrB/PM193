92272131e84d000107d00a1b406ea4e0d6fe60cf
AndyArrB <122043862@upq.edu.mx>
2025-06-12 16:07:03 -0600

    Práctica 5. Avance pt1

--- Blank/App.js
+++ Blank/App.js
@@ -1,15 +1,32 @@
+/* ZONA 1: IMPORTACIONES */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
+const Texto = (props) => {
+  const {contenido} = props
+  return (
+    <Text> {contenido} </Text>
+  )
+}
+
+/* ZONA 2: MAIN */
 export default function App() {
   return (
+    /* Todos los componentes deben ir dentro de un view */
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo,"></Texto>
+      <Texto contenido="Lol"></Texto>
+      <button title="Presionar"> Presionar </button>
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+
+/* ZONA 3: ESTILOS Y ESTÉTICA */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
